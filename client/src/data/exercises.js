95e4469e3093324bac474c408365ff0d6e2971e4
Alex. <alex.liska04@seznam.cz>
2026-02-13 14:01:29 +0100

    feat: add another exercise for eval

--- client/src/data/exercises.js
+++ client/src/data/exercises.js
@@ -16,3 +16,13 @@ export const SQUAT_TIERS = [
     { label: "Pistol Squat", animation: "/videos/pistol_squat.mp4" },
     { label: "Shrimp Squat", animation: "/videos/shrimp_squat.mp4" },
 ];
+
+export const PULLUP_TIERS = [
+    {label: "Scapular Pull-up", animation: "/videos/scapular_pullup.mp4"},
+    {label: "Band Assisted Pull-up", animation: "/videos/band_assisted_pullup.mp4"},
+    {label: "Negative Pull-up", animation: "/videos/negative_pullup.mp4"},
+    {label: "Standard Pull-up", animation: "/videos/standard_pullup.mp4"},
+    {label: "Archer Pull-up", animation: "/videos/archer_pullup.mp4"},
+    {label: "One Hand Pull-up", animation: "/videos/one_hand_pullup.mp4"},
+]
+    
