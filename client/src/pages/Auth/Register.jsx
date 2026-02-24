db69095fa15f20017887850a89f6275e2d186b6c
Alex. <alex.liska04@seznam.cz>
2026-02-24 16:54:37 +0100

    fix: register fetching from localhost

--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -22,7 +22,7 @@ export default function Register() {
                 return;
             }
 
-            const response = await fetch("http://localhost:5000/api/register", {
+            const response = await fetch(`${import.meta.env.REACT_APP_API_URL}/api/register`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ username, email, password }),
