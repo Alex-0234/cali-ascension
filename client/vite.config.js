461a4644d404aa242e9b4e17825db32e49de492d
Alex Liška <alex.liska04@seznam.cz>
2026-02-28 14:29:24 +0100

    feat: implement pwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Calistenics Ascension',
+      short_name: 'Cali Ascension',
+      description: 'An RPG-like calisthenics workout tracker',
+      theme_color: '#16161a',
+      background_color: '#0a0a0a',
+      display: 'standalone',
+      icons: [
+        {
+          src: 'icons/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'icons/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+  })],
 })
