cc2f1e5111728b1393a421fc542b7b26c0452e7e
Alex. <alex.liska04@seznam.cz>
2026-03-04 13:20:13 +0100

    feat: implement exercise history deletion

--- client/src/components/ui/workoutHistoryBlock.jsx
+++ client/src/components/ui/workoutHistoryBlock.jsx
@@ -3,7 +3,17 @@ import { EXERCISE_DB } from "../../data/exercise_db";
 
 export default function WorkoutHistoryBlock({ onClose }) {
     const userData = useUserStore((state) => state.userData);
+    const setUserData = useUserStore((state) => state.setUserData);
     const workoutHistory = userData.workoutHistory || [];
+
+
+    const handleExerciseDelete = (entry) => {
+        const filteredHistory = workoutHistory.filter(obj => obj !== entry);
+        console.log(filteredHistory);
+        setUserData({
+            workoutHistory: [...filteredHistory]
+        })
+    }
 
     return (
         <div className="workout-history-block generic-border">
@@ -14,10 +24,12 @@ export default function WorkoutHistoryBlock({ onClose }) {
                 {workoutHistory.length === 0 ? (
                     <p>No workout history available.</p>
                 ) : (
-
+                    
                     workoutHistory.map((entry, index) => (
-                        <div key={index} className="workout-entry generic-border">
-                            {console.log('Workout Entry:', entry.exerciseID, entry.date, entry.sets)}
+
+                        <div key={index} className="workout-entry generic-border" style={{position: 'relative'}}>
+                                <div style={{position: 'absolute', top: '1rem', right: '1rem', color: 'red'}} onClick={() => handleExerciseDelete(entry)}>Delete</div>
+
                             <p><strong>Date:</strong> {new Date(entry.date).toLocaleDateString()}</p>
                             <p><strong>Exercise:</strong> {EXERCISE_DB[entry.exerciseID].name}</p>
                             <p><strong>Total Reps:</strong> {entry.totalReps}</p>
