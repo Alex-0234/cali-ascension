92bc6c240978c13c12cb3dd661e7abb582082cc4
Alex. <alex.liska04@seznam.cz>
2026-03-05 21:16:21 +0100

    refactor: slight css and inline style changes + html icon folder href change

--- client/src/components/ui/workoutHistoryBlock.jsx
+++ client/src/components/ui/workoutHistoryBlock.jsx
@@ -1,7 +1,7 @@
 import useUserStore from "../../store/usePlayerStore";
 import { EXERCISE_DB } from "../../data/exercise_db";
 
-export default function WorkoutHistoryBlock({ onClose, onDelete }) {
+export default function WorkoutHistoryBlock({ onClose }) {
     const userData = useUserStore((state) => state.userData);
     const setUserData = useUserStore((state) => state.setUserData);
     const workoutHistory = userData.workoutHistory || [];
@@ -20,7 +20,7 @@ export default function WorkoutHistoryBlock({ onClose, onDelete }) {
                 <h3>Workout History</h3>
                 <div className="btn-close" onClick={onClose}>x</div>
 
-                <div style={{width: 'calc(100% - 2rem)', height: '100%', overflow: 'auto'}}>
+                <div style={{width: '100%', height: '100%', overflow: 'auto'}}>
                 {workoutHistory.length === 0 ? (
                     <p>No workout history available.</p>
                 ) : (
@@ -28,19 +28,24 @@ export default function WorkoutHistoryBlock({ onClose, onDelete }) {
                     workoutHistory.map((entry, index) => (
 
                         <div key={index} className="workout-entry generic-border" style={{position: 'relative'}}>
-                                <div style={{position: 'absolute', top: '1rem', right: '1rem', color: 'red'}} onClick={() => {handleExerciseDelete(entry) }}>Delete</div>
-
-                            <p><strong>Date:</strong> {new Date(entry.date).toLocaleDateString()}</p>
-                            <p><strong>Exercise:</strong> {EXERCISE_DB[entry.exerciseID].name}</p>
-                            <p><strong>Total Reps:</strong> {entry.totalReps}</p>
-                            <p><strong>Sets:</strong></p>
-                            <ul>
-                                {entry.sets.map((set, setIndex) => (
-                                    <li key={setIndex}>
-                                        Reps: {set.reps}, Extra Weight: {set.extraWeight} kg
-                                    </li>   
-                                ))}
-                            </ul>
+                                <div style={{position: 'absolute', top: '1rem', right: '1rem', color: 'red'}} onClick={() => handleExerciseDelete(entry)}>Delete</div>
+                            <div>
+                                <p><strong>Date:</strong> {new Date(entry.date).toLocaleDateString()}</p>
+                                <p><strong>Exercise:</strong> {EXERCISE_DB[entry.exerciseID].name}</p>
+                            </div>
+                            <div style={{width: '100%', height: '1px', background: 'white'}}></div>
+                            <div>
+                            <p style={{margin: ' 1rem 0 1rem 0', fontSize:'clamp(1rem, 3vw, 1.4rem)'}}><strong>Total Reps:</strong> {entry.totalReps}</p>
+                            <p style={{margin: 0}}><strong>Sets:</strong></p>
+                                <ul style={{margin: 0}}>
+                                    {entry.sets.map((set, setIndex) => (
+                                        <li key={setIndex} style={{ fontSize: 'clamp(0.8rem, 2vw, 1.2rem)'}}>
+                                            Reps: {set.reps}, Extra Weight: {set.extraWeight} kg
+                                        </li>   
+                                    ))}
+                                </ul>
+                            </div>
+                            
                         </div>
                     
                     ))
