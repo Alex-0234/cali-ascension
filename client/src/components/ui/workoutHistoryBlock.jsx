52333167541002e1f4e2bf968684fac57f23be2b
Alex. <alex.liska04@seznam.cz>
2026-03-23 11:31:54 +0100

    fix: fix the workout history not working

--- client/src/components/ui/workoutHistoryBlock.jsx
+++ client/src/components/ui/workoutHistoryBlock.jsx
@@ -7,18 +7,23 @@ import styles from '../../styles/history.module.css'
 
 export default function WorkoutHistoryBlock() {
     const userData = useUserStore((state) => state.userData);
-    const setUserData = useUserStore((state) => state.setUserData);
     const setHistory = useUIStore((state) => state.setHistory);
 
     const workoutHistory = userData.workoutHistory || [];
+    let safeWorkoutHistory = Array.isArray(workoutHistory) ? {} : workoutHistory;
 
-    const handleExerciseDelete = (indexToRemove) => {
-        const filteredHistory = workoutHistory.filter((_, index) => index !== indexToRemove);
-        setUserData({
-            ...userData,
-            workoutHistory: filteredHistory
-        });
-    };
+    let tempHistory = {};
+    let numErr = 0;
+    Object.keys(safeWorkoutHistory).map(date => {
+        if (date.split('-').length !== 3) {
+            numErr += 1;
+        }
+        else {
+            tempHistory[date] = safeWorkoutHistory[date];
+        }
+    })
+    safeWorkoutHistory = tempHistory;
+    console.log(safeWorkoutHistory)
 
     return (
         <section className={styles.workoutBlock}>
@@ -29,39 +34,47 @@ export default function WorkoutHistoryBlock() {
             </div>
 
             <div className={styles.wrapper}>
-                {workoutHistory.length === 0 ? (
+                {safeWorkoutHistory.length === 0 ? (
                     <p style={{textAlign: 'center', color: '#64748b', marginTop: '2rem'}}>
                         No workout history available yet.
                     </p>
                 ) : (
-                    workoutHistory.map((entry, index) => (
+                    Object.keys(safeWorkoutHistory).map((date, index) => (
                         <div key={index} className={styles.workoutEntry}>
-                            
-                            <div className={styles.deleteBtn} onClick={() => handleExerciseDelete(index)}>
-                                DELETE
-                            </div>
-                            
-                            <div className={styles.entryStats}>
-                                <p><strong>Date:</strong> {new Date(entry.date).toLocaleDateString()}</p>
-                                <p><strong>Exercise:</strong> {EXERCISE_DB[entry.exerciseID]?.name || "Unknown"}</p>
+                            <div>
+                                <h2>{date}</h2>
+                                <p className={styles.totalReps}>
+                                    <strong>Total Reps:</strong> {workoutHistory[date].totalVolume} <br/>
+                                    <strong>Total Sets:</strong> {workoutHistory[date].totalSets}
+                                </p>
                             </div>
                             
                             <div className={styles.entryDivider}></div>
                             
                             <div>
-                                <p className={styles.totalReps}>
-                                    <strong>Total Reps:</strong> {entry.totalReps}
-                                </p>
+                                
                                 <p style={{margin: '0 0 0.5rem 0'}}><strong>Sets:</strong></p>
                                 
-                                <ul className={styles.setsList}>
-                                    {entry.sets.map((set, setIndex) => (
-                                        <li key={setIndex}>
-                                            Set {setIndex + 1}: {set.reps} reps 
-                                            {set.extraWeight > 0 ? ` (+ ${set.extraWeight} kg)` : ''}
-                                        </li>   
+                               <ul className={styles.setsList}>
+                                    {console.log(safeWorkoutHistory[date])}
+                                    { Object.keys(safeWorkoutHistory[date].exercises).map((ex, exIndex) => (
+                                        <div key={exIndex}> 
+                                        <h2>{ex}</h2>
+                                        <ul>
+                                            {safeWorkoutHistory[date].exercises[ex].sets.map((set, setIndex) => {
+                                            return (
+                                            <li key={`${ex}-${setIndex}`}>
+                                                Set {setIndex + 1}: {set.reps} reps 
+                                                {set.extraWeight > 0 ? ` (+ ${set.extraWeight} kg)` : ''}
+                                            </li>  
+                                            )
+                                            })}
+                                          
+                                        </ul>
+
+                                        </div>
                                     ))}
-                                </ul>
+                                </ul> 
                             </div>
                             
                         </div>
