3965ece64ba706bd5aa515736cbac22c91a5b9fb
Alex. <alex.liska04@seznam.cz>
2026-03-25 21:58:46 +0100

    refactor: fixing button color

--- client/src/components/ui/backBtn.jsx
+++ client/src/components/ui/backBtn.jsx
@@ -5,7 +5,7 @@ const BackButton = ({onClick, position, align, top, left, stroke}) => {
     return (
 
         <button  onClick={onClick} style={{position: position, placeSelf: align, background: 'transparent', border: 'none', top: top, left: left }}>
-            <svg width="24" stroke={stroke} height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <svg width="24" stroke={stroke} height="24" viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M15 18l-6-6 6-6" />
             </svg>
         </button>
